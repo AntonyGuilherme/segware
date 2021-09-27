2ec05ba9ff2dae00d4af7e734866227897e96f87
=Antony <guilhermecosta994@gmail.com>
2021-09-27 11:55:27 -0300

    Projeto Inicial
    Biblioteca :
    *  h2
    * spring-data
    * spring-development-tools

---
+++ postandvote/src/main/java/br/com/segware/postandvote/PostandvoteApplication.java
@@ -0,0 +1,13 @@
+package br.com.segware.postandvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostandvoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostandvoteApplication.class, args);
+	}
+
+}
